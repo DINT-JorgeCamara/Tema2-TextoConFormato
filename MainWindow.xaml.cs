25ef083c6d7263ea8155a459e1e327e365f702c8
JorgeCamaraH <jorge.camara@iesdoctorbalmis.com>
2022-09-30 10:11:03 +0200

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tema2_CaracteresLimitados
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextoUsuarioTexBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextoConFormato.Text = TextoUsuarioTexBox.Text;
+        }
+        private void CheckBox_onClick(object sender, RoutedEventArgs e)
+        {
+            if (NegritaCheckBox.IsChecked == true)
+                TextoConFormato.FontWeight = FontWeights.Bold;
+            else
+                TextoConFormato.FontWeight = FontWeights.Normal;
+
+            if (CursivaCheckBox.IsChecked == true)
+                TextoConFormato.FontStyle = FontStyles.Italic;
+            else
+                TextoConFormato.FontStyle = FontStyles.Normal;
+        }
+
+        private void ColoresRadioButton_onClick(object sender, RoutedEventArgs e)
+        {
+            if (AzulRadioButton.IsChecked == true)
+                TextoConFormato.Foreground = Brushes.Blue;
+            else if (RojoRadioButton.IsChecked == true)
+                TextoConFormato.Foreground = Brushes.Red;
+            else TextoConFormato.Foreground = Brushes.Green;
+        }
+    }
+}
